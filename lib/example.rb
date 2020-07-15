a8675e3e19f17c478b56c19ee9af7da2684b95d5
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-15 16:39:46 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -22,4 +22,14 @@ puts phrase
 
 def say_hello_world_ten_times
   pharse = "Hello World!"
-  
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
